a339a69530d19e3be2796fb4374a60af3ff6794f
Elgatha Lee <elgathalee@Elgathas-MacBook-Pro.local>
2014-12-19 23:22:35 -0500

    Goal 7

---
+++ wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,34 @@
+/*
+ * Name: Elgatha Lee
+ * Date: December 19, 2014
+ * Class: PWA
+ * Assignment: Goal7
+ */
+
+<!-- -->
+
+/** declare global variables: */
+    /** "names" array - names of (5) people */
+    /** "instances" max number person object (3) */
+    /** "people" empty array */
+    /** "interval" empty variable */
+
+<!-- -->
+
+/** create functions including:
+   /** "populateHTML"  */
+      // /** use innerHTML to inject names and jobs of people
+   /** "runUpdate" */
+       // /** create "update" function to loop through each person instance stored in People array */
+
+<!-- -->
+
+/** create a for loop (using "instances" for the comparison)  */
+   // /**create a random number between 1 and 5 (number of names in array) using Math.random, store in variable "someName"  */
+   // /**create Person object (var person = new Person(names[someName], someRow)), store in "people" array */
+   // /**call "populateHTML" function  */
+  // /**don't allow duplicate names to appear on the web page */
+
+<!-- -->
+
+/** call "runUpdate" function, set to run 30 times per second */
---
+++ wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -0,0 +1,31 @@
+/*
+ * Name: Elgatha Lee
+ * Date: December 19, 2014
+ * Class: PWA
+ * Assignment: Goal7
+ */
+
+<!-- -->
+
+/** add Person to global window (window.Person = Person;) */
+
+<!-- -->
+
+/** declare variables including: */
+     /** "actions", array actions a person can do */
+	 /** "jobs", array - 4 or more jobs */
+
+<!-- -->
+   
+/** create Person constructor function with: */
+	 /** "job", this.job Math.random */
+     /** "row", this.row  row */
+     /** "name", this.name name of the person */
+     /** "action", this.action Math.random */
+  
+<!-- -->
+
+    /** set initial action */
+<!-- -->
+
+/** use "update" function from main.js file to change action of person and write change to column 3 */
